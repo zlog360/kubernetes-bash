45f3a58e7ae854b87b5ca1996908bcbfd9fe8779
zeeshanalisyed <zeeshan_asyed@hotmail.com>
2020-10-13 16:19:25 +0400

    added dashboard info

--- startup/start_kuberenetes_server.sh
+++ startup/start_kuberenetes_server.sh
@@ -4,5 +4,5 @@ kubeadm init --apiserver-advertise-address=$MASTER_IP --pod-network-cidr=$POD_NE
 mkdir -p $HOME/.kube &&
 sudo rm -r $HOME/.kube/config &&
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config &&
-sudo chown $(id -u):$(id -g) $HOME/.kube/config &&
+sudo chown $USER:$USER $HOME/.kube/config &&
 kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
