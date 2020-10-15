0523339c8e65782ec1e61c3b0d4e0e8ae0d5ec42
zeeshanalisyed <zeeshan_asyed@hotmail.com>
2020-10-15 11:34:16 +0400

    Changed Network Instrallation in seuqence

--- installation/env.template.sh
+++ installation/env.template.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export MASTER_IP=your_ip
 export POD_NET_CIDR=ypur_pod_network
+export USER=emallates
--- startup/start_kuberenetes_server.sh
+++ startup/start_kuberenetes_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo $MASTER_IP:$POD_NET_CIDR 
 kubeadm init --apiserver-advertise-address=$MASTER_IP --pod-network-cidr=$POD_NET_CIDR  --ignore-preflight-errors=all --v=5 &&
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml &&
 mkdir -p $HOME/.kube &&
 sudo rm -r $HOME/.kube/config &&
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config &&
-sudo chown $USER:$USER $HOME/.kube/config &&
-kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+sudo chown $USER:$USER $HOME/.kube/config
